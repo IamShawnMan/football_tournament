0566c78182ab37790a884d7ab8ecc306fe860aeb
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2025-04-10 20:32:49 +0500

    Clubs CRUD created

--- src/controllers/clubController.js
+++ src/controllers/clubController.js
@@ -14,7 +14,87 @@ export const createClub = async (req, res, next) => {
         newClub,
       },
     });
-  } catch (error) {}
+  } catch (error) {
+    return res.send(error);
+  }
 };
 
-export const getAllClubs = async (req, res, next) => {};
+export const getAllClubs = async (req, res, next) => {
+  try {
+    const allClubs = await Club.find();
+
+    res.json({
+      status: "success",
+      message: "All clubs",
+      error: null,
+      data: {
+        allClubs,
+      },
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const getClubById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const clubById = await Club.findById(id);
+    if (!clubById) {
+      return res.status(404).json({
+        message: "Club not found",
+      });
+    }
+    res.json({
+      status: "success",
+      message: "club by id",
+      error: null,
+      data: {
+        clubById,
+      },
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const deleteClub = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletingClub = await Club.findById(id);
+    if (!deletingClub) {
+      return res.send("Club not found");
+    }
+    await Club.deleteOne({
+      _id: id,
+    });
+    res.json({
+      status: "success",
+      message: "Club deleted",
+      error: null,
+      data: null,
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
+
+export const editClub = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const editingClub = await Club.findById(id);
+    if (!editingClub) {
+      return res.send("Club not found");
+    }
+    const data = { ...req.body };
+    await Club.updateOne({ _id: id }, data);
+    res.json({
+      status: "success",
+      message: "Club edited",
+      error: null,
+      data: null,
+    });
+  } catch (error) {
+    return res.send(error);
+  }
+};
--- src/routers/clubRoutes.js
+++ src/routers/clubRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { createClub } from "../controllers/index.js";
+import {
+  createClub,
+  deleteClub,
+  editClub,
+  getAllClubs,
+  getClubById,
+} from "../controllers/index.js";
 
 const router = express.Router();
 
-router.post("/", createClub);
+router
+  .post("/", createClub)
+  .get("/", getAllClubs)
+  .get("/:id", getClubById)
+  .delete("/:id", deleteClub)
+  .put("/:id", editClub);
 
 export default router;
